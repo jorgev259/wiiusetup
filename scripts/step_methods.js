99fb2fe459ba85bb01fca6661b500cdf22eb6bd1
rikumax25 <jorgev259@outlook.es>
2017-04-22 20:58:58 -0600

    Updated steps

--- scripts/step_methods.js
+++ scripts/step_methods.js
@@ -11,24 +11,26 @@ function startup_CFW(){
     getLatestRelease("vgmoose","hbas",".zip","Homebrew App Store");
     extractZip("Homebrew App Store","wiiu","");
     
+    getLatestRelease("dimox789","homebrew_launcher","homebrew_launcher.v1.4.zip","Homebrew Launcher 1.4");
+    extractZip("Homebrew Launcher 1.3","","");
+    
     getLatestRelease("FIX94","haxchi","CBHC","CBHC");
     extractZip("CBHC","","");
 
     getLatestRelease("FIX94","haxchi","Haxchi","Haxchi");
     deletefile_zip("Haxchi","haxchi/config.txt");
     extractZip("Haxchi","","");
+    
     getFileBuffer_url("https://wiiu.guide/images/config.txt", "Config.txt");       
-
-    addFile("Config.txt","haxchi","config.txt","list");
-    
-    getFileBuffer_url("https://cors-anywhere.herokuapp.com/https://github.com/dimok789/homebrew_launcher/releases/download/v1.3/homebrew_launcher.v1.3.zip","Homebrew Launcher 1.3");
-    extractZip("Homebrew Launcher 1.3","","");
-    
+    addFile("Config.txt","haxchi","config.txt","list");    
     getLatestRelease("dimok789","homebrew_launcher","channel","Homebrew Launcher Channel");
-    extractZip("Homebrew Launcher Channel","install","");
+    extractZip("Homebrew Launcher Channel","install/hbc","");
     
     notLatestRelease("dimok789","mocha",".zip","Mocha CFW");
     getFileBuffer_zip("Mocha CFW","mocha.elf","mocha.elf","wiiu/apps/mocha");
+             
+    getLatestRelease("Ryuzaki-MrL","savemii",".zip","savemii")
+    extractZip("savemii","wiiu/apps","")
     
     getLatestRelease("Yardape8000","wupinstaller",".zip","WUP installer");
     extractZip("WUP installer","","");
@@ -36,19 +38,21 @@ function startup_CFW(){
     getLatestRelease("koolkdev","disc2app",".zip","disc2app");
     extractZip("disc2app","","");
     
-    getFileBuffer_url("https://cors-anywhere.herokuapp.com/http://wiiubru.com/appstore/zips/wuphax.zip", "WUPhax");
-    extractZip("WUPhax","","");
+    getLatestRelease("Maschell","hid_to_vpad",".zip","HiD to VPad");
+    extractZip("Hid to VPad","","");
     
-    getLatestRelease("Maschell","saviine",".zip","Saviine");
-    extractZip("Saviine","","");
+    getFileBuffer_url("https://cors-anywhere.herokuapp.com/http://wiiubru.com/appstore/zips/nnupatcher.zip", "NNU-Patcher");
+    extractZip("NNU-Patcher","","");
+    
+    getFileBuffer_url("https://cors-anywhere.herokuapp.com/http://wiiubru.com/appstore/zips/nnupatcher.zip", "WUPhax");
+    extractZip("WUPhax","","");
+     getFileBuffer_url("https://wiiu.guide/images/vWii_cIOS_apps_20131218.zip", "vWii cIOS apps");
+    extractZip("vWii cIOS apps","","");
+    
+    getFileBuffer_url("https://wiiu.guide/images/Patched_IOS80_Installer_for_vWii.zip","Patched IOS80 installer"); 
+    extractFolder("Patched IOS80 installer","apps","");
     
     //getFileBuffer_url("https://cors-anywhere.herokuapp.com/https://bootmii.org/get.php?file=hackmii_installer_v1.2.zip", "BootMii");
     //getFileBuffer_zip("BootMii","boot.elf","boot.elf","");
-    
-    getFileBuffer_url("https://wiiu.guide/images/vWii_cIOS_apps_20131218.zip", "vWii cIOS apps");
-    extractZip("vWii cIOS apps","","");
-    
-    
-   getFileBuffer_url("https://wiiu.guide/images/Patched_IOS80_Installer_for_vWii.zip","Patched IOS80 installer"); 
-    extractFolder("Patched IOS80 installer","apps","");
+
 }
